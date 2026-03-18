8812ad23817eb6eacd2448873942e941b1d4bcf8
Roberto Sánchez <roberto@espaidinversions.com>
2026-03-18 15:42:01 +0100

    feat: add routing for /investments/funds and /investments/companies, remove InvestmentsIndex

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx";
-import InvestmentsIndex from "./components/InvestmentsIndex.jsx";
+import FundsIndex from "./components/FundsIndex.jsx";
+import CompaniesIndex from "./components/CompaniesIndex.jsx";
 import FundDetail from "./components/FundDetail.jsx";
 import CompanyDetail from "./components/CompanyDetail.jsx";
 
@@ -9,7 +10,9 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/investments" element={<InvestmentsIndex />} />
+      <Route path="/investments" element={<Navigate to="/investments/funds" replace />} />
+      <Route path="/investments/funds" element={<FundsIndex />} />
+      <Route path="/investments/companies" element={<CompaniesIndex />} />
       <Route path="/fund/:id" element={<FundDetail />} />
       <Route path="/company/:id" element={<CompanyDetail />} />
     </Routes>
